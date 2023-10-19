c302ab13b5b70c27af1ea14834e4079624fe07b0
lanya-dorkin <danya.lorkin@gmail.com>
2023-10-19 08:35:59 +0300

    add class CounVectorizer

---
+++ hw_3/test_case.py
@@ -0,0 +1,12 @@
+from countvectorizer import CountVectorizer
+
+
+if __name__ == '__main__':
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+    ]
+    vectorizer = CountVectorizer()
+    count_matrix = vectorizer.fit_transform(corpus)
+    print(vectorizer.get_feature_names())
+    print(count_matrix)
